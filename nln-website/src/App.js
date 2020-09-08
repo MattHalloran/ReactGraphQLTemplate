6e319611b75e845374742e1e3281cb6db4d785d5
Matthew Halloran <mdhalloran@yahoo.com>
2020-09-08 10:43:34 -0400

    Reactified Navbar

--- nln-website/src/App.js
+++ nln-website/src/App.js
@@ -1,55 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
+import Navbar from './components/Navbar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
 
-      <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
-        <div class="container">
-          <a class="navbar-brand" href="#">Start Bootstrap</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="#">Sign Up</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Log In</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navbar/>
 
-      <header class="masthead text-center text-white">
-        <div class="masthead-content">
-          <div class="container">
-            <h1 class="masthead-heading mb-0">One Page Wonder</h1>
-            <h2 class="masthead-subheading mb-0">Will Rock Your Socks Off</h2>
-            <a href="#" class="btn btn-primary btn-xl rounded-pill mt-5">Learn More</a>
+      <header className="masthead text-center text-white">
+        <div className="masthead-content">
+          <div className="container">
+            <h1 className="masthead-heading mb-0">One Page Wonder</h1>
+            <h2 className="masthead-subheading mb-0">Will Rock Your Socks Off</h2>
+            <a href="#" className="btn btn-primary btn-xl rounded-pill mt-5">Learn More</a>
           </div>
         </div>
-        <div class="bg-circle-1 bg-circle"></div>
-        <div class="bg-circle-2 bg-circle"></div>
-        <div class="bg-circle-3 bg-circle"></div>
-        <div class="bg-circle-4 bg-circle"></div>
+        <div className="bg-circle-1 bg-circle"></div>
+        <div className="bg-circle-2 bg-circle"></div>
+        <div className="bg-circle-3 bg-circle"></div>
+        <div className="bg-circle-4 bg-circle"></div>
       </header>
 
       <section>
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-lg-6 order-lg-2">
-              <div class="p-5">
-                <img class="img-fluid rounded-circle" src="img/01.jpg" alt=""/>
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6 order-lg-2">
+              <div className="p-5">
+                <img className="img-fluid rounded-circle" src="img/01.jpg" alt=""/>
               </div>
             </div>
-            <div class="col-lg-6 order-lg-1">
-              <div class="p-5">
-                <h2 class="display-4">For those about to rock...</h2>
+            <div className="col-lg-6 order-lg-1">
+              <div className="p-5">
+                <h2 className="display-4">For those about to rock...</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
               </div>
             </div>
@@ -58,16 +42,16 @@ function App() {
       </section>
 
       <section>
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-lg-6">
-              <div class="p-5">
-                <img class="img-fluid rounded-circle" src="img/02.jpg" alt=""/>
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6">
+              <div className="p-5">
+                <img className="img-fluid rounded-circle" src="img/02.jpg" alt=""/>
               </div>
             </div>
-            <div class="col-lg-6">
-              <div class="p-5">
-                <h2 class="display-4">We salute you!</h2>
+            <div className="col-lg-6">
+              <div className="p-5">
+                <h2 className="display-4">We salute you!</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
               </div>
             </div>
@@ -76,16 +60,16 @@ function App() {
       </section>
 
       <section>
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-lg-6 order-lg-2">
-              <div class="p-5">
-                <img class="img-fluid rounded-circle" src="img/03.jpg" alt=""/>
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6 order-lg-2">
+              <div className="p-5">
+                <img className="img-fluid rounded-circle" src="img/03.jpg" alt=""/>
               </div>
             </div>
-            <div class="col-lg-6 order-lg-1">
-              <div class="p-5">
-                <h2 class="display-4">Let there be rock!</h2>
+            <div className="col-lg-6 order-lg-1">
+              <div className="p-5">
+                <h2 className="display-4">Let there be rock!</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
               </div>
             </div>
@@ -93,9 +77,9 @@ function App() {
         </div>
       </section>
 
-      <footer class="py-5 bg-black">
-        <div class="container">
-          <p class="m-0 text-center text-white small">Copyright &copy; Your Website 2020</p>
+      <footer className="py-5 bg-black">
+        <div className="container">
+          <p className="m-0 text-center text-white small">Copyright &copy; Your Website 2020</p>
         </div>
 
       </footer>
---
+++ nln-website/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+        <div className="container">
+          <a className="navbar-brand" href="#">Start Bootstrap</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="#">Sign Up</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Log In</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
+}
