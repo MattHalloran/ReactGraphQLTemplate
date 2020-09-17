160a957dc93992dd2787588a8f3c5d47ad09c22f
Matthew Halloran <mdhalloran@yahoo.com>
2020-09-17 15:39:29 -0400

    Texting added
    The email script has been upgraded to allow messages to be sent to phone numbers

--- nln-website/api/messenger.py
+++ nln-website/api/messenger.py
@@ -1,8 +1,8 @@
 # --------------------------------------------
-# Generates and sends emails
+# Generates and sends emails/texts
 # Initially created with the help of https://www.youtube.com/watch?v=JRCJ6RtE3xU
 # Arguments
-#   email_addresses - who should receive this email
+#   recipients - who should receive this email
 #   to_text - This changes what the 'To' field of the email says. 
 #             If left blank, the emails will be used
 #   message - This sets the text of the message. This is optional, depending on 
@@ -17,38 +17,82 @@ import os
 import sys
 import smtplib
 from email.message import EmailMessage
+from twilio.rest import Client
+import difflib
 
 PORT_NUM = 465
 # Stored in environment variables so it's not uploaded to GitHub. 
-print(os.environ)
 EMAIL_USERNAME = os.environ.get("AFA_EMAIL_USERNAME") #What you login as
 EMAIL_FROM = os.environ.get("AFA_EMAIL_FROM") #Who the recipient thinks the email is from
 EMAIL_PASSWORD = os.environ.get("AFA_EMAIL_PASSWORD")
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
+
+twilio_client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+carrier_remove_characters = ['-', '.', '&']
+carrier_dict = {
+    "tmobile": "tmomail.net",
+    "virgin Mobile": "vmobl.com",
+    "att": "txt.att.net",
+    "sprint": "messaging.sprintpcs.com",
+    "verizon Wireless": "vtext.com",
+    "tracfone": "mmst5.tracfone.com",
+    "ting": "message.ting.com",
+    "boost mobile": "myboostmobile.com",
+    "us cellular": "email.uscc.net",
+    "metro pcs": "mymetropcs.com"
+}
+
+#Returns the email associated with a phone number. Currently only implemented for the US
+#Arguments:
+#1) number - The phone number. This should preferrably include the country code (ex: '+16098675309')
+def numberToEmail(number:str):
+    #Already an email
+    if '@' in number:
+        return number
+    carrier_info = twilio_client.lookups.phone_numbers(number).fetch(type=['carrier'])
+    if carrier_info.carrier['type'] != 'mobile':
+        raise Exception('Cannot send a text message to a landline!')
+    #Using the carrier string from twilio, try to match it to the carrier dict. 
+    #Since we don't know the exact format twilio is using, we must find the closest match
+    carrier = carrier_info.carrier['name'].lower()
+    for character in carrier_remove_characters:
+        carrier = carrier.replace(character, '')
+    closest_carriers = difflib.get_close_matches(carrier, carrier_dict)
+    if len(closest_carriers) < 1:
+        raise Exception('Could not find the carrier for this number!')
+    carrier_extension = carrier_dict[closest_carriers[0]]
+    #Return {number without country code}@{carrier email extension}
+    return f'{number[-10:]}@{carrier_extension}'
 
 #Arguments:
-#email_addresses - a list of recipients
+#recipients - a list of recipients. Can contain phone numbers or emails
 #to_text - Who the recipients will think the email is sent to. Useful for bcc. Defaults to the list of recipients, but can actually be any text
 #subject - The subject of the message
 #message - The actual message, in plaintext. If sending an html file instead (such as a newsletter), it is still a good
 #idea to set this field in case some recipients have disabled HTML emails
 #html_file - Path to an html file, if sending one
-def main(email_addresses:list, to_text:str = '', subject:str = '', message:str = '', html_file:str = ''):
+def main(recipients:list, to_text:str = '', subject:str = '', message:str = '', html_file:str = ''):
 
-    if len(email_addresses) == 0:
+    if len(recipients) == 0:
         print('No email addresses passed in')
         return
 
+    #Convert any phone numbers into emails
+    recipients = [numberToEmail(x) for x in recipients]
+
     if to_text == '':
-        if len(email_addresses) > 1:
-            to_text = ', '.join(email_addresses)
+        if len(recipients) > 1:
+            to_text = ', '.join(recipients)
         else:
-            to_text = email_addresses[0]
+            to_text = recipients[0]
 
     msg = EmailMessage()
     msg['Subject'] = subject
     msg['From'] = EMAIL_FROM
     msg['To'] = to_text
-    msg['Bcc'] = email_addresses #So recipients don't see other email addresses
+    msg['Bcc'] = recipients #So recipients don't see other email addresses
     msg.set_content(message)
     
     if html_file == '':
@@ -65,7 +109,7 @@ def main(email_addresses:list, to_text:str = '', subject:str = '', message:str =
     with smtplib.SMTP_SSL('smtp.gmail.com', PORT_NUM) as smtp:
         try:
             smtp.login(EMAIL_USERNAME, EMAIL_PASSWORD)
-            to_string = ', '.join(email_addresses)
+            to_string = ', '.join(recipients)
             print(f'Sending email to {to_string}')
             smtp.send_message(msg)
         except Exception as e:
@@ -77,4 +121,4 @@ if __name__ == '__main__':
     if len(sys.argv) > num_args_required:
         main(sys.argv[1], sys.argv[2])
     else:
-        main(['matthalloran8@gmail.com'], 'recipients', 'this is a subject', 'this is a message')
+        main(['matthalloran8@gmail.com', '+16095796202'], 'recipients', 'this is a subject', 'this is a message')
