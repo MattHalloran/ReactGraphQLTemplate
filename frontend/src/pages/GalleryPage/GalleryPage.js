348a6f2d72337da5778e941816267576a76a20c4
Matthew Halloran <matthalloran8@gmail.com>
2021-03-10 20:49:55 -0500

    Gallery page
    Gallery page shows images as expected

--- frontend/src/pages/GalleryPage/GalleryPage.js
+++ frontend/src/pages/GalleryPage/GalleryPage.js
@@ -24,7 +24,7 @@ function GalleryPage() {
     const urlParams = useParams();
     // [base64 data, alt]
     const [curr_img, setCurrImg] = useState(null);
-    const curr_index = useMemo(() => images_meta.current?.findIndex(m => m.id === urlParams.img) ?? -1, [images_meta, urlParams]);
+    const curr_index = useMemo(() => images_meta.current?.findIndex(m => m.id == urlParams.img) ?? -1, [images_meta, urlParams]);
     const loading = useRef(false);
     const track_scrolling_id = 'scroll-tracked';
     //Estimates how many images will fill the screen
@@ -50,16 +50,20 @@ function GalleryPage() {
         // First check if the image data has already been retrieved
         if (index in full_images.current) {
             setCurrImg(full_images.current[index]);
-        } else {
+        } else if (index >= 0) {
             loading_full_image.current = true;
+            console.log(full_images.current);
             // Request the image from the backend
             getImage(id, 'l').then(response => {
+                console.log('GOT IMAGE RESPONSE', response);
                 let image_data = [`data:image/jpeg;base64,${response.image}`, response.alt];
                 setCurrImg(image_data);
                 full_images.current[index] = image_data;
             }).catch(error => {
                 console.error('FAILED TO LOAD FULL IMAGE', error)
             }).finally(() => loading_full_image.current = false);
+        } else {
+            setCurrImg(null);
         }
     }, [loading_full_image, full_images])
 
@@ -95,7 +99,7 @@ function GalleryPage() {
                 let img = response.images[i];
                 let new_data = {
                     "key": meta.id,
-                    "src": `data:image/jpeg;base64,${img.src}`,
+                    "src": `data:image/jpeg;base64,${img}`,
                     "width": meta.width,
                     "height": meta.height,
                 };
