cce353ca985e093265f49355c42de125d8ac813d
Matthew Halloran <matthalloran8@gmail.com>
2021-05-06 14:25:11 -0400

    Upload availability rewritten for node
    The upload availability functionality has now been implemented in node, rather than Python. It is still untested, but should be somewhat close to working

--- backend/worker/uploadAvailability/queue.js
+++ backend/worker/uploadAvailability/queue.js
@@ -1,12 +1,14 @@
 import Bull from 'bull';
 import uploadAvailabilityProcess from './process';
+import xlsx from 'node-xlsx';
 
 const uploadAvailabilityQueue = new Bull('uploadAvailability');
 uploadAvailabilityQueue.process(uploadAvailabilityProcess);
 
-export function uploadAvailability(to=[], body) {
+export function uploadAvailability(data_buffer) {
+    const parsed = xlsx.parse(data_buffer);
+    const rows = parsed[0].data;
     uploadAvailabilityQueue.add({
-        to: to,
-        body: body
+        rows: rows
     });
 }
