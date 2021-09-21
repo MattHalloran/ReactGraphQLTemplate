c0654af751e32e9f28330bc61de4f3b394d90e5f
Matthew Halloran <mdhalloran@yahoo.com>
2021-09-21 16:05:27 -0400

    🗺Organized navigational logic
    Common logic related to a user's navigational options have been organized and placed into the userActions.js file.

--- packages/ui/src/utils/userActions.js
+++ packages/ui/src/utils/userActions.js
@@ -1,4 +1,5 @@
 import {
+    ExitToApp as ExitToAppIcon,
     Person as PersonIcon,
     PersonAdd as PersonAddIcon,
     Settings as SettingsIcon,
@@ -8,17 +9,20 @@ import {
 import { ROLES } from '@local/shared';
 import { LINKS } from 'utils';
 import _ from 'lodash';
+import { initializeApollo } from 'graphql/utils/initialize';
+import { logoutMutation } from 'graphql/mutation';
+import {
+    Badge,
+    BottomNavigationAction,
+    Button,
+    IconButton,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+} from '@material-ui/core';
 
-// Returns user actions, in a list of this format:
-//  [
-//      label: str,
-//      value: str,
-//      link: str,
-//      onClick: func,
-//      icon: Material-UI Icon,
-//      number of notifications: int,
-//  ]
-export function getUserActions(session, userRoles, cart) {
+// Returns navigational actions available to the user
+export function getUserActions({ session, userRoles, cart, exclude = [] }) {
     let actions = [];
 
     // If someone is not logged in, display sign up/log in links
@@ -32,8 +36,80 @@ export function getUserActions(session, userRoles, cart) {
         }
         actions.push(['Shop', 'shop', LINKS.Shopping, null, ShopIcon, 0],
             ['Profile', 'profile', LINKS.Profile, null, PersonIcon, 0],
-            ['Cart', 'cart', LINKS.Cart, null, ShoppingCartIcon, cart?.items?.length ?? 0]);
+            ['Cart', 'cart', LINKS.Cart, null, ShoppingCartIcon, cart?.items?.length ?? 0],
+            ['Log out', 'logout', LINKS.Home, () => { const client = initializeApollo(); client.mutation({ mutatation: logoutMutation }) }, ExitToAppIcon, 0]);
     }
 
-    return actions;
+    return actions.map(a => createAction(...a)).filter(a => !exclude.includes(a.value));
+}
+
+// Factory for creating action objects
+export const createAction = (label, value, link, onClick, Icon, numNotifications) => ({
+    label,
+    value,
+    link,
+    onClick,
+    Icon,
+    numNotifications,
+})
+
+// Display actions as a list
+export const actionsToList = ({ actions, history, classes = { listItem: {}, listItemIcon: {} }, showIcon = true, onAnyClick }) => {
+    return actions.map(({ label, value, link, onClick, Icon, numNotifications }) => (
+        <ListItem
+            key={value}
+            className={classes.listItem}
+            button
+            onClick={() => {
+                history.push(link);
+                if (onClick) onClick();
+                if (onAnyClick) onAnyClick();
+            }}>
+            {showIcon && Icon ?
+                (<ListItemIcon>
+                    <Badge badgeContent={numNotifications} color="error">
+                        <Icon className={classes.listItemIcon} />
+                    </Badge>
+                </ListItemIcon>) : null}
+            <ListItemText primary={label} />
+        </ListItem>
+    ))
+}
+
+// Display actions in a horizontal menu
+export const actionsToMenu = ({ actions, history, classes = { button: {} } }) => {
+    return actions.map(({ label, value, link, onClick }) => (
+        <Button
+            key={value}
+            variant="text"
+            size="large"
+            className={classes.button}
+            onClick={() => { history.push(link); if (onClick) onClick() }}
+        >
+            {label}
+        </Button>
+    ));
+}
+
+// Display actions in a bottom navigation
+export const actionsToBottomNav = ({ actions, history, classes = { action: {} } }) => {
+    return actions.map(({ label, value, link, onClick, Icon, numNotifications }) => (
+        <BottomNavigationAction
+            key={value}
+            className={classes.action}
+            label={label}
+            value={value}
+            onClick={() => { history.push(link); if (onClick) onClick() }}
+            icon={<Badge badgeContent={numNotifications} color="error"><Icon /></Badge>} />
+    ))
+}
+
+// Display an action as an icon button
+export const actionToIconButton = ({ action, history, classes = { button: {} } }) => {
+    const { value, link, Icon, numNotifications } = action;
+    return <IconButton className={classes.button} edge="start" color="inherit" aria-label={value} onClick={() => history.push(link)}>
+        <Badge badgeContent={numNotifications} color="error">
+            <Icon />
+        </Badge>
+    </IconButton>
 }
